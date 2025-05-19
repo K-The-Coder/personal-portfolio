f641350157d5d76b42979b52710187f6df0f215d
Keketso Mabule <kekemabule19990216@gmail.com>
2025-05-19 19:38:20 +0200

    Made nav-bar responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,56 @@
+import { useEffect, useRef, useState } from "react"
 
 export default function Header(){
+
+    const [isOpen, setIsOpen] = useState(false)
+    const navRef = useRef(null)
+    const toggleRef = useRef(null)
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth > 600){
+                setIsOpen(false)
+            }
+        }
+
+        const handleClickOutside = (event) => {
+            if (isOpen && 
+                navRef.current && 
+                !navRef.current.contains(event.target) && 
+                toggleRef.current && 
+                !toggleRef.current.contains(event.target)){
+                setIsOpen(false)
+            }
+        }
+
+        document.addEventListener('mousedown', handleClickOutside)
+        window.addEventListener('resize', handleResize)
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside)
+            window.removeEventListener('resize', handleResize)
+        }
+
+    }, [isOpen])
+
     return (
         <header>
             <div className="logo">KM</div>
+            <div className="menu-toggle" onClick={() => setIsOpen(!isOpen)} ref={toggleRef}>&#9776;</div>
             <nav className="nav-bar">
                 <ul>
                     <li><a href="#hero-section">Home</a></li>
                     <li><a href="#about-section">About</a></li>
                     <li><a href="#projects-section">Projects</a></li>
                     <li><a href="#contact-section">Contact</a></li>
+                </ul>
+            </nav>
+            <nav className={`mobile-nav-bar ${isOpen ? 'open' : ''}`} ref={navRef}>
+                <ul>
+                    <li onClick={() => setIsOpen(!isOpen)}><span className="close-nav-bar-button">x</span></li>
+                    <li onClick={() => setIsOpen(false)}><a href="#hero-section">Home</a></li>
+                    <li onClick={() => setIsOpen(false)}><a href="#about-section" >About</a></li>
+                    <li onClick={() => setIsOpen(false)}><a href="#projects-section" >Projects</a></li>
+                    <li onClick={() => setIsOpen(false)}><a href="#contact-section" >Contact</a></li>
                 </ul>
             </nav>
         </header>
