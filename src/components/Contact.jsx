cccc073d3dcab8b1329b118816b393268926a6d8
Keketso Mabule <kekemabule19990216@gmail.com>
2025-06-05 18:01:38 +0200

    Added content to Contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,6 +2,14 @@ export default function Contact() {
     return (
         <>
             <h3>Contact Me</h3>
+            <form className="contact-form">
+                <div className="input-group">
+                    <input type="text" name="full-name" id="full-name" placeholder="Full Name..."/>
+                    <input type="email" name="email-address" id="email-address" placeholder="Email Address..." />
+                </div>
+                <textarea name="message" id="message" placeholder="Send a Message..."></textarea>
+                <button type="submit">Submit</button>
+            </form>
         </>
     )
 }
