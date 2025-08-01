1c33ef509864b2cf98b56382305b41e8618b7149
Keketso Mabule <kekemabule19990216@gmail.com>
2025-08-01 17:20:36 +0200

    rectified dependencies

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,5 @@
 export default function Button({
-  type = "",
+  type = "button",
   children,
   onClick = () => {},
 }) {
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,5 +1,5 @@
 export default function Input({
-  type = "",
+  type = "text",
   name = "",
   id = "",
   placeholder = "",
